2e86f7493875f023505c77faeed4e2b3f3c7f208
heyuanw <heyuan_wang@hotmail.com>
2025-05-05 14:03:50 -0700

    Add user authentication using Djoser

--- restaurant/views.py
+++ restaurant/views.py
@@ -1,22 +1,24 @@
 from django.shortcuts import render
-from rest_framework import generics, viewsets, permissions
-from .models import Menu, Booking
-from .serializers import MenuSerializer, BookingSerializer
+from rest_framework import generics, viewsets, decorators
+from rest_framework.permissions import IsAuthenticated
+from .models import MenuItem, Booking
+from .serializers import MenuItemSerializer, BookingSerializer
 
 # Create your views here.
 def index(request):
     return render(request, 'index.html', {})
 
 class MenuItemsView(generics.ListCreateAPIView):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
+    permission_classes = [IsAuthenticated]
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
 
 
 class SingleMenuItemView(generics.RetrieveUpdateAPIView, generics.DestroyAPIView):
-    queryset = Menu.objects.all()
-    serializer_class = MenuSerializer
+    queryset = MenuItem.objects.all()
+    serializer_class = MenuItemSerializer
 
 class BookingViewSet(viewsets.ModelViewSet):
+    permission_classes = [IsAuthenticated]
     queryset = Booking.objects.all()
     serializer_class = BookingSerializer
-    # permission_classes = [permissions.IsAuthenticated]
